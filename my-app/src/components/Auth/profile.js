e30b5f0883d486f6ecc588c5eb43d797b8ff4bf6
Shreyan Satheesh <s0901527@students.katyisd.org>
2025-03-23 09:32:41 -0500

    ahhh

--- my-app/src/components/Auth/profile.js
+++ my-app/src/components/Auth/profile.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import './profile.css'; // Import CSS for styling
 import LoginButton from './LoginButton';
+
 const Profile = () => {
   const { user, isAuthenticated, logout } = useAuth0();
 
   return (
     isAuthenticated && (
       <div className="profile-container">
-        <img className="profile-picture" src={user?.picture} alt={user?.name} />
+        <img
+          className="profile-picture"
+          src={user && user.picture ? user.picture : ''}
+          alt={user && user.name ? user.name : 'Profile Picture'}
+        />
         <div className="profile-info">
-          <h2>{user?.name}</h2>
-          <p>{user?.email}</p>
+          <h2>{user && user.name ? user.name : 'No Name'}</h2>
+          <p>{user && user.email ? user.email : 'No Email'}</p>
         </div>
         <LoginButton/>
       </div>
